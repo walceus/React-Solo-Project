e6a1adcec5960866ab9d40ddfb6bbc7126f099d4
walceus <widalceus@gmail.com>
2021-12-07 15:44:57 -0500

    small issuses and restoreUser

--- authenticate-me/frontend/src/components/LoginFormPage/index.js
+++ authenticate-me/frontend/src/components/LoginFormPage/index.js
@@ -3,6 +3,7 @@ import * as sessionActions from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 
+
 function LoginFormPage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
